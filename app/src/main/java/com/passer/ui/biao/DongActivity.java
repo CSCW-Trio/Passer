7575a9629b8ea81393bb86d17d280c4b213786e5
younger96 <474203566@qq.com>
2017-04-16 14:05:04 +0800

    Young Change Base3

---
+++ app/src/main/java/com/passer/ui/biao/DongActivity.java
@@ -0,0 +1,8 @@
+package com.passer.ui.biao;
+
+/**
+ * Created by 47420 on 2017/4/16.
+ */
+
+public class DongActivity {
+}
---
+++ app/src/main/java/com/passer/ui/dong/BiaoActivity.java
@@ -0,0 +1,8 @@
+package com.passer.ui.dong;
+
+/**
+ * Created by 47420 on 2017/4/16.
+ */
+
+public class BiaoActivity {
+}
